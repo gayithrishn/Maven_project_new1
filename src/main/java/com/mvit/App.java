5894356f5820d5906b64deacdc8d72fe33beb4a1
gayithrishn <gayithrishn@gmail.com>
2025-04-12 15:33:23 +0530

    second updated feature

--- src/main/java/com/mvit/App.java
+++ src/main/java/com/mvit/App.java
@@ -9,5 +9,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        for(i=0;i<=10;i++)
+        {
+            if (i==2)
+            {
+                System.out.println("wlcome to devops lab");
+            }
+
+        }
+        System.out.println("print i values:" + i);
     }
 }
